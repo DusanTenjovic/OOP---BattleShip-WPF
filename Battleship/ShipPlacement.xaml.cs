5062ffd0ffcb48bc5730430b0fd1a1ea51bd795f
LazarNedelja <lazalaki069@gmail.com>
2021-06-08 22:48:29 +0200

    Doradjen kostur

--- Battleship/ShipPlacement.xaml.cs
+++ Battleship/ShipPlacement.xaml.cs
@@ -18,11 +18,13 @@ namespace Battleship
     /// Interaction logic for ShipPlacement.xaml
     /// </summary>
     public partial class ShipPlacement : UserControl
-    {
+    { 
         public event EventHandler play;
         Engine engine;
         List<Button> sviDugmici = new List<Button>();
-        int igracNaPotezu = 0;
+
+        double screenWidth = System.Windows.SystemParameters.PrimaryScreenWidth;
+        double screenHeight = System.Windows.SystemParameters.PrimaryScreenHeight;
 
         public ShipPlacement(Engine engine)
         {
@@ -34,18 +36,18 @@ namespace Battleship
 
         void postaviDugmice(int velicina)
         {
-            int vel = 30;
+            int vel = 70;
             for (int i = 0; i < velicina; i++)
             {
                 for (int j = 0; j < velicina; j++)
                 {
                     Button b = new Button()
                     {
-                        Height = 30,
-                        Width = 30,
+                        Height = vel,
+                        Width = vel,
                         VerticalAlignment = 0,
                         HorizontalAlignment = 0,
-                        Margin = new Thickness(50 + vel * i, 50 + vel * j, 0, 0),
+                        Margin = new Thickness(screenWidth/15 + vel * i, screenHeight/15 + vel * j, 0, 0),
                         Content = i.ToString()                        
                     };
                     b.Click += btnClick;
@@ -60,7 +62,7 @@ namespace Battleship
         {
             if (velicina == 8)
             {
-                
+                nacrtajDugme2();
             }
             else if (velicina == 10)
             {
@@ -87,11 +89,62 @@ namespace Battleship
         {
             Button b = new Button()
             {
+                Height = screenHeight / 38.4,
+                Width = screenWidth / 19.2,
+                VerticalAlignment = 0,
+                HorizontalAlignment = 0,
+                Margin = new Thickness(screenWidth * 5 / 7, screenHeight * 5 / 7, 0, 0),
+                Content = "dvojka"
+            };
+            grid.Children.Add(b);
+        }
+        
+        void nacrtajDugme3()
+        {
+            Button b = new Button()
+            {
+                Height = 100,
+                Width = 300,
+                VerticalAlignment = 0,
+                HorizontalAlignment = 0,
+                Margin = new Thickness(50, 50, 0, 0),
+                Content = "trojka"
+            };
+        }
+        void nacrtajDugme4()
+        {
+            Button b = new Button()
+            {
                 Height = 30,
                 Width = 30,
                 VerticalAlignment = 0,
                 HorizontalAlignment = 0,
-                Margin = new Thickness(50 , 50 , 0, 0)            
+                Margin = new Thickness(50, 50, 0, 0),
+                Content = "cetvorka"
+            };
+        }
+        void nacrtajDugme5()
+        {
+            Button b = new Button()
+            {
+                Height = 30,
+                Width = 30,
+                VerticalAlignment = 0,
+                HorizontalAlignment = 0,
+                Margin = new Thickness(50, 50, 0, 0),
+                Content = "petica"
+            };
+        }
+        void nacrtajDugme6()
+        {
+            Button b = new Button()
+            {
+                Height = 30,
+                Width = 30,
+                VerticalAlignment = 0,
+                HorizontalAlignment = 0,
+                Margin = new Thickness(50, 50, 0, 0),
+                Content = "sestica"
             };
         }
     }
